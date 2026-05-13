6e8761b9f61a9fa6e5205c6cb7ffd413893346f2
SirSouza <sr.souza.contato@gmail.com>
2026-05-13 18:15:31 -0300

    feat: support multiple packages in a single command

--- yuki.c
+++ yuki.c
@@ -10,26 +10,25 @@ struct Command
 };
 
 struct Command commands[] = {
-
-    {"install", "pacman -S %s", 1},
-    {"remove", "pacman -Rns %s", 1},
-    {"search", "pacman -Ss %s", 1},
-    {"update", "pacman -Syu", 0},
-    {"info", "pacman -Si %s", 1},
-    {"info-local", "pacman -Qi %s", 1},
-    {"list", "pacman -Q", 0},
-    {"list-search", "pacman -Qs %s", 1},
-    {"orphans", "pacman -Qdt", 0},
-    {"clean", "pacman -Sc", 0},
-    {"sync", "pacman -Syy", 0}
-
+    {"install",     "pacman -S %s",   1},
+    {"remove",      "pacman -Rns %s", 1},
+    {"search",      "pacman -Ss %s",  1},
+    {"update",      "pacman -Syu",    0},
+    {"info",        "pacman -Si %s",  1},
+    {"info-local",  "pacman -Qi %s",  1},
+    {"list",        "pacman -Q",      0},
+    {"list-search", "pacman -Qs %s",  1},
+    {"orphans",     "pacman -Qdt",    0},
+    {"clean",       "pacman -Sc",     0},
+    {"sync",        "pacman -Syy",    0}
 };
 
 int main(int argc, char *argv[])
 {
-  char command[256];
-
+  char packages[512];
+  char command[768];
   int total = sizeof(commands) / sizeof(commands[0]);
+
   if (argc < 2)
   {
     printf("use: yuki <command> <package>\n");
@@ -47,12 +46,20 @@ int main(int argc, char *argv[])
           printf("use: yuki <command> <package>\n");
           return 1;
         }
-        snprintf(command, sizeof(command), commands[i].pacman, argv[2]);
+         packages[0] = '\0';
+          for (int j = 2; j < argc; j++)
+        {
+          if (j > 2) strncat(packages, " ", sizeof(packages) - strlen(packages) - 1);
+          strncat(packages, argv[j], sizeof(packages) - strlen(packages) - 1);
+        }
+
+        snprintf(command, sizeof(command), commands[i].pacman, packages);
       }
       else
       {
         snprintf(command, sizeof(command), "%s", commands[i].pacman);
       }
+
       system(command);
       return 0;
     }
