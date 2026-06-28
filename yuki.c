74f909e9fe5b3ea7923c443a0b97d1982993bcf2
SirSouza <sr.souza.contato@gmail.com>
2026-06-28 17:07:23 -0300

    add command to clean orphans dependencies

--- yuki.c
+++ yuki.c
@@ -10,18 +10,19 @@ struct Command
 };
 
 struct Command commands[] = {
-    {"install",     "pacman -S %s",   1},
+    {"install",     "pacman -S %s", 1},
     {"remove",      "pacman -Rns %s", 1},
-    {"search",      "pacman -Ss %s",  1},
-    {"update",      "pacman -Syu",    0},
-    {"info",        "pacman -Si %s",  1},
-    {"info-local",  "pacman -Qi %s",  1},
-    {"list-all",        "pacman -Q",      0},
-    {"list-search", "pacman -Qs %s",  1},
-    {"list-orphans",     "pacman -Qdt",    0},
-    {"clean",       "pacman -Sc",     0},
-    {"sync",        "pacman -Syyu",    0}
-};
+    {"search",      "pacman -Ss %s", 1},
+    {"update",      "pacman -Syu", 0},
+    {"info",        "pacman -Si %s", 1},
+    {"info-local",  "pacman -Qi %s", 1},
+    {"list-all",        "pacman -Q", 0},
+    {"list-search", "pacman -Qs %s", 1},
+    {"list-orphans",     "pacman -Qdt", 0},
+    {"remove-orphans", "pacman -Rns $(pacman -Qdtq)", 0},
+    {"clean-cache",       "pacman -Sc", 0},
+    {"sync",        "pacman -Syyu", 0}
+};s
 
 int main(int argc, char *argv[])
 {
